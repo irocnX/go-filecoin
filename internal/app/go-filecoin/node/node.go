4d4fb608123e3072f58da592225d0b63dab003fa
shannonwells <shannonwells@users.noreply.github.com>
2020-03-18 16:17:58 -0700

    * fully configure retrieval mining submodule in node * extract state viewing functionality from manager * this simplifies FakeStateView(er)s * no need for separate resolver * give RetrievalProtocolSubmodule a client connector

--- internal/app/go-filecoin/node/node.go
+++ internal/app/go-filecoin/node/node.go
@@ -22,8 +22,10 @@ import (
 
 	"github.com/filecoin-project/go-filecoin/internal/app/go-filecoin/internal/submodule"
 	"github.com/filecoin-project/go-filecoin/internal/app/go-filecoin/paths"
+	"github.com/filecoin-project/go-filecoin/internal/app/go-filecoin/paymentchannel"
 	"github.com/filecoin-project/go-filecoin/internal/app/go-filecoin/plumbing/msg"
 	"github.com/filecoin-project/go-filecoin/internal/app/go-filecoin/porcelain"
+
 	"github.com/filecoin-project/go-filecoin/internal/pkg/block"
 	"github.com/filecoin-project/go-filecoin/internal/pkg/cborutil"
 	"github.com/filecoin-project/go-filecoin/internal/pkg/chain"
@@ -502,6 +504,17 @@ func (node *Node) setupRetrievalMining(ctx context.Context) error {
 	if err != nil {
 		return errors.Wrap(err, "failed to get mining address")
 	}
+
+	waiter := msg.NewWaiter(node.chain.ChainReader, node.chain.MessageStore, node.Blockstore.Blockstore, node.Blockstore.CborStore)
+
+	mgrStateViewer := paymentchannel.NewManagerStateViewer(node.Chain().ChainReader, node.Blockstore.CborStore)
+	paychMgr := paymentchannel.NewManager(
+		ctx,
+		node.Repo.Datastore(),
+		waiter,
+		node.Messaging.Outbox,
+		mgrStateViewer)
+
 	rp, err := submodule.NewRetrievalProtocolSubmodule(
 		node.Blockstore.Blockstore,
 		node.Repo.Datastore(),
@@ -509,9 +522,10 @@ func (node *Node) setupRetrievalMining(ctx context.Context) error {
 		node.Host(),
 		providerAddr,
 		node.Wallet.Wallet,
-		nil, // TODO: payment channel manager API, in follow-up
+		paychMgr,
 		node.PieceManager(),
 	)
+
 	if err != nil {
 		return errors.Wrap(err, "failed to build node.RetrievalProtocol")
 	}
